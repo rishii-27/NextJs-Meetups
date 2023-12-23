e711b0e77dfd6cb4676644ccbddc05135e76b1b9
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-12-23 18:18:16 +0530

    Working on pre-Render through states and Implemented Show Detail button

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1689784626180-3ae58cba794a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      address="some street"
+      description="first meet"
+    />
+  );
+}
+
+export default MeetupDetails;
