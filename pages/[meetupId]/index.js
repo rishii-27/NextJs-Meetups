e6b8e65f6ca1c7995d0be8c307e3e8bcc375409d
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-12-26 13:31:41 +0530

    Added getStaticProps and getStaticPaths to genrate static page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,4 +10,39 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.unsplash.com/photo-1689784626180-3ae58cba794a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        id: meetupId,
+        address: "some street",
+        description: "first meet",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
